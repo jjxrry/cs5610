230cff8b44e6e3e5288ce63b27adebe826f462f9
jjxrry <114961129+jjxrry@users.noreply.github.com>
2024-09-10 12:24:47 -0700

    finish 3.5

--- src/kanbas/courses/index.tsx
+++ src/kanbas/courses/index.tsx
@@ -1,7 +1,29 @@
+import { Modules } from "./modules";
+import { CoursesNavigation } from "./Navigation";
+import { Navigate, Route, Routes } from "react-router";
+
 export const Courses = () => {
     return (
       <div id="wd-courses">
         <h2>Course 1234</h2>
+        <hr />
+            <table>
+                <tr>
+                    <td valign="top">
+                        <CoursesNavigation />
+                    </td>
+                    <td valign="top">
+                        <Routes>
+                        <Route path="/" element={<Navigate to="home" />} />
+                        <Route path="home" element={<h2>Home</h2>} />
+                        <Route path="modules" element={<Modules />} />
+                        <Route path="assignments" element={<h2>Assignments</h2>} />
+                        <Route path="assignments/:aid" element={<h2>Assignment Editor</h2>} />
+                        <Route path="people" element={<h2>People</h2>} />
+                        </Routes>
+                    </td>
+                </tr>
+            </table>
       </div>
   );}
   
