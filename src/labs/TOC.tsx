d67428ba7982ee3a071f1d26112f0caf5ac334e9
jjxrry <114961129+jjxrry@users.noreply.github.com>
2024-09-17 16:08:44 -0700

    on 3.3.17

--- src/labs/TOC.tsx
+++ src/labs/TOC.tsx
@@ -1,13 +1,20 @@
 import { Link } from "react-router-dom";
+// import { useLocation } from "react-router";
 
 export const TOC = () => {
     return (
-      <ul>
-        <li><Link to="/labs">Labs</Link></li>
-        <li><Link to="/labs/lab1">Lab 1</Link></li>
-        <li><Link to="/labs/lab2">Lab 2</Link></li>
-        <li><Link to="/labs/lab3">Lab 3</Link></li>
-        <li><Link to="/kanbas">Kanbas</Link></li>
+      <ul className="nav nav-pills">
+        <li className="nav-item">
+          <Link to="/labs">Labs</Link>
+        </li>
+        <li className="nav-item"><Link to="/labs/lab1">Lab 1</Link>
+        </li>
+        <li className="nav-item"><Link to="/labs/lab2">Lab 2</Link>
+        </li>
+        <li className="nav-item"><Link to="/labs/lab3">Lab 3</Link>
+        </li>
+        <li className="nav-item"><Link to="/kanbas">Kanbas</Link>
+        </li>
       </ul>
     );
   }
