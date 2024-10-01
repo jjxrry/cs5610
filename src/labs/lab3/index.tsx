2bee2d895fcda592ede1edd0869e7d999fefc219
jjxrry <114961129+jjxrry@users.noreply.github.com>
2024-10-01 13:50:25 -0700

    finish 2.2.2

--- src/labs/lab3/index.tsx
+++ src/labs/lab3/index.tsx
@@ -1,8 +1,13 @@
+import { VariablesAndConstants } from "./VariablesAndConstants";
+import { VariableTypes } from "./VariableTypes";
+
 export const Lab3 = () => {
     return (
-      <div>
-        <h2>Lab 3</h2>
-      </div>
+        <div id="wd-lab3" className="container-fluid">
+            <h2>Lab 3</h2>
+            <VariablesAndConstants />
+            <VariableTypes />
+        </div>
     );
-  }
-  
+}
+
