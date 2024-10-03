eaf8200374fdd3b34130f3740d27217f0d4e8b73
jjxrry <114961129+jjxrry@users.noreply.github.com>
2024-10-03 14:00:11 -0700

    finish 2.6

--- src/labs/index.tsx
+++ src/labs/index.tsx
@@ -1,21 +1,22 @@
 import { Lab1 } from "./lab1";
 import { Route, Routes, Navigate } from "react-router";
-import { TOC }from "./TOC";
+import { TOC } from "./TOC";
 import { Lab2 } from "./lab2";
 import { Lab3 } from "./lab3";
 
 export const Labs = () => {
-  return (
-    <div id="wd-labs">
-      <h1>Labs</h1>
-      <h2>Jerry Gao</h2>
-      <p>Section 20595 | 7PM (EST) Thursday Lecture</p>
-      <TOC />
-      <Routes>
-        <Route path="/" element={<Navigate to="lab1" />} />
-        <Route path="lab1" element={<Lab1 />} />
-        <Route path="lab2" element={<Lab2 />} />
-        <Route path="lab3" element={<Lab3 />} />
-      </Routes>
-    </div>
-);}
+    return (
+        <div id="wd-labs">
+            <h1>Labs</h1>
+            <h2>Jerry Gao</h2>
+            <p>Section 20595 | 7PM (EST) Thursday Lecture</p>
+            <TOC />
+            <Routes>
+                <Route path="/" element={<Navigate to="lab1" />} />
+                <Route path="lab1" element={<Lab1 />} />
+                <Route path="lab2" element={<Lab2 />} />
+                <Route path="lab3/*" element={<Lab3 />} />
+            </Routes>
+        </div>
+    );
+}
--- src/labs/lab3/index.tsx
+++ src/labs/lab3/index.tsx
@@ -1,7 +1,9 @@
+import { Add } from "./Add";
 import { AddingAndRemovingToFromArrays } from "./AddingAndRemovingToFromArrays";
 import { ArrayIndexAndLength } from "./ArrayIndexAndLength";
 import { ArrowFunctions } from "./ArrowFunctions";
 import { BooleanVariables } from "./BooleanVariables";
+import { Classes } from "./Classes";
 import { ConditionalOutputIfElse } from "./ConditionalOutputIfElse";
 import { ConditionalOutputInline } from "./ConditionalOutputInline";
 import { Destructing } from "./Destructing";
@@ -10,14 +12,18 @@ import { FilterFunction } from "./FilterFunction";
 import { FindFunction } from "./FindFunction";
 import { FindIndex } from "./FindIndex";
 import { ForLoops } from "./ForLoops";
+import { Highlight } from "./Highlight";
 import { House } from "./House";
 import { IfElse } from "./IfElse";
 import { ImpliedReturn } from "./ImpliedReturn";
 import { JsonStringify } from "./JsonStringify";
 import LegacyFunctions from "./LegacyFunctions";
 import { MapFunction } from "./MapFunction";
+import { PathParameters } from "./PathParameters";
 import { SimpleArrays } from "./SimpleArrays";
 import { Spreading } from "./Spreading";
+import { Square } from "./Square";
+import { Styles } from "./Styles";
 import { TemplateLiterals } from "./TemplateLiterals";
 import { TernaryOperator } from "./TernaryOperator";
 import { TodoItem } from "./todos/TodoItem";
@@ -27,8 +33,9 @@ import { VariableTypes } from "./VariableTypes";
 
 export const Lab3 = () => {
     return (
-        <div id="wd-lab3" className="container-fluid">
+        <div id="wd-lab3" className="container">
             <h2>Lab 3</h2>
+            <h3>JavaScript</h3>
             <VariablesAndConstants />
             <VariableTypes />
             <BooleanVariables />
@@ -55,6 +62,17 @@ export const Lab3 = () => {
             <Spreading />
             <Destructing />
             <DestructingImports />
+            <Classes />
+            <Styles />
+            <Add a={3} b={4} />
+            <h4>Square of 4</h4>
+            <Square>4</Square>
+            <hr />
+            <Highlight>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
+                vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
+            </Highlight>
+            <PathParameters />
         </div>
     );
 }
