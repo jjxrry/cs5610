5f7a3d9c0c57cc5b8dd527bac41667ad6ef93a81
jjxrry <114961129+jjxrry@users.noreply.github.com>
2024-10-07 14:24:32 -0700

    change routes

--- src/kanbas/Navigation.tsx
+++ src/kanbas/Navigation.tsx
@@ -7,11 +7,11 @@ import { Link, useLocation } from "react-router-dom"
 export const KanbasNavigation = () => {
     const { pathname } = useLocation()
     const links = [
-        { label: "Dashboard", path: "/Kanbas/Dashboard", icon: AiOutlineDashboard },
-        { label: "Courses", path: "/Kanbas/Dashboard", icon: LiaBookSolid },
-        { label: "Calendar", path: "/Kanbas/Calendar", icon: IoCalendarOutline },
-        { label: "Inbox", path: "/Kanbas/Inbox", icon: FaInbox },
-        { label: "Labs", path: "/Labs", icon: LiaCogSolid },
+        { label: "Dashboard", path: "/kanbas/dashboard", icon: AiOutlineDashboard },
+        { label: "Courses", path: "/kanbas/dashboard", icon: LiaBookSolid },
+        { label: "Calendar", path: "/kanbas/calendar", icon: IoCalendarOutline },
+        { label: "Inbox", path: "/kanbas/inbox", icon: FaInbox },
+        { label: "Labs", path: "/labs", icon: LiaCogSolid },
     ]
     return (
         <div id="wd-kanbas-navigation" style={{ width: 115 }}
@@ -20,15 +20,15 @@ export const KanbasNavigation = () => {
                 className="list-group-item bg-black border-0 mt-4 text-center">
                 <img src="/images/neu-logo.png" alt="northeastern logo" width="75px" />
             </a><br />
-            <Link to="/Kanbas/Account" className={`list-group-item text-center border-0 bg-black
-            ${pathname.includes("Account") ? "bg-white text-danger" : "bg-black text-white"}`}>
+            <Link to="/kanbas/account" className={`list-group-item text-center border-0 bg-black
+            ${pathname.includes("account") ? "bg-white text-danger" : "bg-black text-white"}`}>
                 <FaRegCircleUser className={`fs-1 ${pathname.includes("Account") ? "text-danger" : "text-white"}`} />
                 <br />
                 Account
             </Link>
             {links.map((link) => (
                 <Link key={link.path} to={link.path} className={`list-group-item bg-black text-center border-0
-              ${pathname.includes(link.label) ? "text-danger bg-white" : "text-white bg-black"}`}>
+              ${pathname.includes(link.label.toLowerCase()) ? "text-danger bg-white" : "text-white bg-black"}`}>
                     {link.icon({ className: "fs-1 text-danger" })}
                     <br />
                     {link.label}
