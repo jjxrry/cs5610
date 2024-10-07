9f424e8e7090a318feb66959352ce21e621ce6bd
jjxrry <114961129+jjxrry@users.noreply.github.com>
2024-10-07 14:20:34 -0700

    finish 3.4

--- src/kanbas/courses/Navigation.tsx
+++ src/kanbas/courses/Navigation.tsx
@@ -1,22 +1,17 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useParams } from "react-router-dom";
 export const CoursesNavigation = () => {
+    const { cid } = useParams()
     const { pathname } = useLocation()
-    const links = [
-        { label: "Home", path: "/kanbas/courses/:cid/home" },
-        { label: "Modules", path: "/kanbas/courses/:cid/modules" },
-        { label: "Piazza", path: "/kanbas/courses/:cid/piazza" },
-        { label: "Zoom", path: "/kanbas/courses/:cid/zoom" },
-        { label: "Assignments", path: "/kanbas/courses/:cid/assignments" },
-        { label: "Quizzes", path: "/kanbas/courses/:cid/quizzes" },
-        { label: "Grades", path: "/kanbas/courses/:cid/grades" },
-        { label: "People", path: "/kanbas/courses/:cid/people" },
-    ]
+    const links = ["Home", "Modules", "Piazza", "Zoom", "Assignments", "Quizzes", "Grades", "People"]
+    const getPath = (link: string) => {
+        return `/kanbas/courses/${cid}/${link.toLowerCase()}`
+    }
     return (
         <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
             {links.map((link) => (
-                <Link to={link.path} key={link.path} className={`list-group-item text-danger border border-0
-                ${pathname.includes(link.label.toLowerCase()) ? "active text-black" : "text-danger"}`}>
-                    {link.label}
+                <Link to={getPath(link)} key={link} className={`list-group-item text-danger border border-0
+                ${pathname.includes(link.toLowerCase()) ? "active text-black" : "text-danger"}`}>
+                    {link}
                 </Link>
             ))}
         </div>
