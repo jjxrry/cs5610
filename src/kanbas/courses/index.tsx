cfb8f462ce117f85116ff85f64a2162fb7740a89
jjxrry <114961129+jjxrry@users.noreply.github.com>
2024-10-10 13:06:30 -0700

    finish 3.8

--- src/kanbas/courses/index.tsx
+++ src/kanbas/courses/index.tsx
@@ -4,18 +4,19 @@ import { AssignmentEditor } from "./assignments/Editor";
 import { Home } from "./home";
 import { Modules } from "./modules";
 import { CoursesNavigation } from "./Navigation";
-import { Navigate, Route, Routes, useParams } from "react-router";
+import { Navigate, Route, Routes, useParams, useLocation } from "react-router";
 import { PeopleTable } from "./people/Table";
 import { courses } from "../database";
 
 export const Courses = () => {
     const { cid } = useParams()
+    const { pathname } = useLocation()
     const course = courses.find((course) => course._id === cid)
     return (
         <div id="wd-courses">
             <h2 className="text-danger">
                 <FaAlignJustify className="me-4 fs-4 mb-1" />
-                {course && course.name}
+                {course && course.name} &gt; {pathname.split('/')[4].charAt(0).toUpperCase() + pathname.split('/')[4].slice(1)}
             </h2>
             <hr />
             <div className="d-flex">
