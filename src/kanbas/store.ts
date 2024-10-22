66bd591f8c512fcbbb5da238f11eaddd7ba4b04b
jjxrry <114961129+jjxrry@users.noreply.github.com>
2024-10-22 14:32:01 -0700

    add

--- src/kanbas/store.ts
+++ src/kanbas/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { modulesReducer } from "./courses/modules/reducer";
 import { accountReducer } from "./account/reducer";
+import { assignmentsReducer } from "./courses/assignments/reducer";
 
 export const store = configureStore({
     reducer: {
         modulesReducer,
         accountReducer,
+        assignmentsReducer,
     },
 });
